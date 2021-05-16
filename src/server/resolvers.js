101ed0df846b1d45a27829fc62b28f05cf9f26ed
helgastogova <helgastogova@gmail.com>
2021-05-16 11:21:00 +0300

    update

--- src/server/resolvers.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  Query: {
-    launches: (_, __, {dataSources}) => dataSources.launchAPI.getAllLaunches(),
-    launch: (_, {id}, {dataSources}) =>
-      dataSources.launchAPI.getLaunchById({launchId: id}),
-    me: (_, __, {dataSources}) => dataSources.userAPI.findOrCreateUser(),
-  },
-};
