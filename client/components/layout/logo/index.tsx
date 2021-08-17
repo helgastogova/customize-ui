e98a9993c5b66dd5798e80460ea7b6d6b6a0564d
helgastogova <helgastogova@gmail.com>
2021-08-17 10:31:07 +0200

    logo

--- client/components/layout/logo/index.tsx
+++ client/components/layout/logo/index.tsx
@@ -3,11 +3,24 @@ import Text from '@ui/text';
 
 import styles from './logo.module.css';
 
+const randomEmojiArray = [
+  '🌱',
+  '🍭',
+  '💡',
+  '🥦',
+  '👩‍💻',
+  '🙀',
+  '🦄',
+  '🍀',
+  '🌈',
+  '🏕',
+];
+
 const Logo: React.FC = () => {
   return (
     <Link to="/" className={styles.root}>
       <Text bold size="18">
-        helgastogova
+        helgastogova {randomEmojiArray[Math.floor(Math.random() * 10)]}
       </Text>
     </Link>
   );
