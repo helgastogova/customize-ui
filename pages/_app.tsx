515e9a64cdedadb9c876f6fc1e66e005f14e83bc
helgastogova@gmail.com <helgastogova@gmail.com>
2021-08-19 21:24:26 +0200

    Create _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type {AppProps} from 'next/app';
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 import {ApolloProvider} from '@apollo/client';
+import {AuthProvider} from '../lib/auth';
 
 const client = new ApolloClient({
   uri: 'https://api-eu-central-1.graphcms.com/v2/cks4k7g3g6fv401z834yf49p1/master',
@@ -12,7 +13,9 @@ import '../styles/globals.css';
 function App({Component, pageProps}: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <AuthProvider>
         <Component {...pageProps} />
+      </AuthProvider>
     </ApolloProvider>
   );
 }
